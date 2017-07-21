c6ce43511106373660ad5b1335e11eee9f32fa90
tshetty1 <tshetty1@uncc.edu>
2017-07-21 17:03:47 -0400

    Bug fixes in myscript.js file

--- myscript.js
+++ myscript.js
@@ -93,14 +93,20 @@ function funcJSONObject(object) {
  * input.
  */
 function funcTextBox(object) {
-	textBoxCount = textBoxCount + 1;
-	var div = document.createElement('DIV');
-	div.setAttribute("name", "textboxdiv");
-	div.setAttribute("id", "textboxdiv");
-	div.innerHTML = '<input type="text" name="inputtext" id="inputtext'
-			+ textBoxCount + '" value="' + object.text + '"> <br>';
-	document.getElementById("main").appendChild(div);
-	return this;
+	if (!object.hasOwnProperty('text')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the text to be entered in the TextBox.";
+	} else {
+		textBoxCount = textBoxCount + 1;
+		var div = document.createElement('DIV');
+		div.setAttribute("name", "textboxdiv");
+		div.setAttribute("id", "textboxdiv");
+		div.innerHTML = '<input type="text" name="inputtext" id="inputtext'
+				+ textBoxCount + '" value="' + object.text + '"> <br>';
+		document.getElementById("main").appendChild(div);
+		return this;
+	}
+
 }
 
 /*
@@ -108,17 +114,24 @@ function funcTextBox(object) {
  * given as input.
  */
 function funcTogglerButtonBox(object) {
-	togglerbuttoncount = togglerbuttoncount + 1;
-	var div = document.createElement('DIV');
-	div.setAttribute("name", "togglerbuttonboxDiv");
-	div.setAttribute("id", "togglerbuttonboxDiv");
-	div.innerHTML = '<button name="togglerbutton" id="togglerbutton'
-			+ togglerbuttoncount + '">' + object.content.text + '</button>';
-	document.getElementById("main").appendChild(div);
-	dynamicObject = object.replacementContent.text;
-	var button = document.getElementById("togglerbutton" + togglerbuttoncount);
-	button.onclick = funcButtonClick;
-	return this;
+	if (!object.hasOwnProperty('content')
+			|| !object.hasOwnProperty('replacementContent')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the content and the replacementContent for the TogglerButtonBox. Both being objects with type and text values.";
+	} else {
+		togglerbuttoncount = togglerbuttoncount + 1;
+		var div = document.createElement('DIV');
+		div.setAttribute("name", "togglerbuttonboxDiv");
+		div.setAttribute("id", "togglerbuttonboxDiv");
+		div.innerHTML = '<button name="togglerbutton" id="togglerbutton'
+				+ togglerbuttoncount + '">' + object.content.text + '</button>';
+		document.getElementById("main").appendChild(div);
+		dynamicObject = object.replacementContent.text;
+		var button = document.getElementById("togglerbutton"
+				+ togglerbuttoncount);
+		button.onclick = funcButtonClick;
+		return this;
+	}
 }
 
 /* This function is called when a TogglerButton is clicked. */
@@ -131,22 +144,28 @@ function funcButtonClick() {
  * as input.
  */
 function funcCheckboxBox(object) {
-	checkboxcount = checkboxcount + 1;
-	var div = document.createElement('DIV');
-	div.setAttribute("name", "checkboxboxdiv");
-	div.setAttribute("id", "checkboxboxdiv");
-	if (dynamicVariable) {
-		div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname'
-				+ checkboxcount + '" checked>' + object.dynamicVariable;
-		document.getElementById("main").appendChild(div);
-	} else if (!dynamicVariable) {
-		div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname'
-				+ checkboxcount + '"/>' + object.dynamicVariable;
-		document.getElementById("main").appendChild(div);
+	if (!object.hasOwnProperty('dynamicVariable')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the value of dynamicVariable for the CheckboxBox.";
+	} else {
+		checkboxcount = checkboxcount + 1;
+		var div = document.createElement('DIV');
+		div.setAttribute("name", "checkboxboxdiv");
+		div.setAttribute("id", "checkboxboxdiv");
+		if (dynamicVariable) {
+			div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname'
+					+ checkboxcount + '" checked>' + object.dynamicVariable;
+			document.getElementById("main").appendChild(div);
+		} else if (!dynamicVariable) {
+			div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname'
+					+ checkboxcount + '"/>' + object.dynamicVariable;
+			document.getElementById("main").appendChild(div);
+		}
+		var checkbox = document.getElementById("checkboxname" + checkboxcount);
+		checkbox.onclick = checkBoxChanged;
+		return this;
 	}
-	var checkbox = document.getElementById("checkboxname" + checkboxcount);
-	checkbox.onclick = checkBoxChanged;
-	return this;
+
 }
 
 /*
@@ -154,21 +173,27 @@ function funcCheckboxBox(object) {
  * JSON Object given as input.
  */
 function funcCheckboxBoxOfRowBox(object) {
-	var div = document.createElement('DIV');
-	div.setAttribute("name", "checkboxboxdiv1");
-	div.setAttribute("id", "checkboxboxdiv1");
-	if (dynamicVariable) {
-		div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname" checked>'
-				+ object.dynamicVariable;
-		document.getElementById("main").appendChild(div);
-	} else if (!dynamicVariable) {
-		div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname"/>'
-				+ object.dynamicVariable;
-		document.getElementById("main").appendChild(div);
+	if (!object.hasOwnProperty('dynamicVariable')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the value of dynamicVariable for the CheckboxBox.";
+	} else {
+		var div = document.createElement('DIV');
+		div.setAttribute("name", "checkboxboxdiv1");
+		div.setAttribute("id", "checkboxboxdiv1");
+		if (dynamicVariable) {
+			div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname" checked>'
+					+ object.dynamicVariable;
+			document.getElementById("main").appendChild(div);
+		} else if (!dynamicVariable) {
+			div.innerHTML = '<input type = "checkbox" name="checkboxname" id="checkboxname"/>'
+					+ object.dynamicVariable;
+			document.getElementById("main").appendChild(div);
+		}
+		var checkbox = document.getElementById("checkboxname");
+		checkbox.onclick = checkBoxOfStyleBoxChanged;
+		return this;
 	}
-	var checkbox = document.getElementById("checkboxname");
-	checkbox.onclick = checkBoxOfStyleBoxChanged;
-	return this;
+
 }
 
 /* This function is called when the CheckBox is clicked. */
@@ -199,26 +224,32 @@ function checkBoxOfStyleBoxChanged() {
  * given as input.
  */
 function funcPaneSelectorBox(object) {
-	paneselectorboxcount = paneselectorboxcount + 1;
-	var div = document.createElement('DIV');
-	div.setAttribute("name", "paneselectorboxdiv");
-	div.setAttribute("id", "paneselectorboxdiv");
-	if (dynamicVariable) {
-		div.innerHTML = '<input type="text" name="paneselectorbox" id="paneselectorbox'
-				+ paneselectorboxcount
-				+ '" value="'
-				+ object.contentTrue.text
-				+ '"> <br>';
-		document.getElementById("main").appendChild(div);
-	} else if (!dynamicVariable) {
-		div.innerHTML = '<input type="text" name="paneselectorbox" id="paneselectorbox'
-				+ paneselectorboxcount
-				+ '" value="'
-				+ object.contentFalse.text
-				+ '"> <br>';
-		document.getElementById("main").appendChild(div);
+	if (!object.hasOwnProperty('dynamicVariable')
+			|| !object.hasOwnProperty('contentTrue')
+			|| !object.hasOwnProperty('contentFalse')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the value of dynamicVariable, contentTrue and contentFalse for the TogglerButtonBox. Both contentTrue and contentFalse are objects with type and text values.";
+	} else {
+		paneselectorboxcount = paneselectorboxcount + 1;
+		var div = document.createElement('DIV');
+		div.setAttribute("name", "paneselectorboxdiv");
+		div.setAttribute("id", "paneselectorboxdiv");
+		if (dynamicVariable) {
+			div.innerHTML = '<input type="text" name="paneselectorbox" id="paneselectorbox'
+					+ paneselectorboxcount
+					+ '" value="'
+					+ object.contentTrue.text + '"> <br>';
+			document.getElementById("main").appendChild(div);
+		} else if (!dynamicVariable) {
+			div.innerHTML = '<input type="text" name="paneselectorbox" id="paneselectorbox'
+					+ paneselectorboxcount
+					+ '" value="'
+					+ object.contentFalse.text + '"> <br>';
+			document.getElementById("main").appendChild(div);
+		}
+		return this;
 	}
-	return this;
+
 }
 
 /*
@@ -226,32 +257,46 @@ function funcPaneSelectorBox(object) {
  * input.
  */
 function funcStyleBox(object) {
-	var type = object.content.type;
-	switch (type) {
-	case "TextBox":
-		funcTextBox(object.content);
-		document.getElementById("inputtext" + textBoxCount).style.fontFamily = object.style.fontFamily;
-		document.getElementById("inputtext" + textBoxCount).style.fontSize = object.style.fontSize
-				+ 'px';
-		break;
-	case "TogglerButtonBox":
-		funcTogglerButtonBox(object.content);
-		document.getElementById("togglerbutton" + togglerbuttoncount).style.fontFamily = object.style.fontFamily;
-		document.getElementById("togglerbutton" + togglerbuttoncount).style.fontSize = object.style.fontSize
-				+ 'px';
-		break;
-	case "CheckboxBox":
-		funcCheckboxBox(object.content);
-		document.getElementById("checkboxlabel" + checkboxcount).style.fontFamily = object.style.fontFamily;
-		document.getElementById("checkboxlabel" + checkboxcount).style.fontSize = object.style.fontSize
-				+ 'px';
-		break;
-	case "PaneSelectorBox":
-		funcPaneSelectorBox(object.content);
-		document.getElementById("paneselectorbox" + paneselectorboxcount).style.fontFamily = object.style.fontFamily;
-		document.getElementById("paneselectorbox" + paneselectorboxcount).style.fontSize = object.style.fontSize
-				+ 'px';
-		break;
+	if (!object.hasOwnProperty('content') || !object.hasOwnProperty('style')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the value of content and style for the StyleBox. content is a Boxdescriptor and style is a JSON object with the properties --> fontFamily and fontSize.";
+	} else {
+		if (!object.style.hasOwnProperty('fontFamily')
+				|| !object.style.hasOwnProperty('fontSize')) {
+			document.getElementById("main").innerHTML = "";
+			document.getElementById("error").innerHTML = "Please enter the fontFamily and fontSize for the style.";
+		} else {
+			var type = object.content.type;
+			switch (type) {
+			case "TextBox":
+				funcTextBox(object.content);
+				document.getElementById("inputtext" + textBoxCount).style.fontFamily = object.style.fontFamily;
+				document.getElementById("inputtext" + textBoxCount).style.fontSize = object.style.fontSize
+						+ 'px';
+				break;
+			case "TogglerButtonBox":
+				funcTogglerButtonBox(object.content);
+				document.getElementById("togglerbutton" + togglerbuttoncount).style.fontFamily = object.style.fontFamily;
+				document.getElementById("togglerbutton" + togglerbuttoncount).style.fontSize = object.style.fontSize
+						+ 'px';
+				break;
+			case "CheckboxBox":
+				funcCheckboxBox(object.content);
+				document.getElementById("checkboxlabel" + checkboxcount).style.fontFamily = object.style.fontFamily;
+				document.getElementById("checkboxlabel" + checkboxcount).style.fontSize = object.style.fontSize
+						+ 'px';
+				break;
+			case "PaneSelectorBox":
+				funcPaneSelectorBox(object.content);
+				document.getElementById("paneselectorbox"
+						+ paneselectorboxcount).style.fontFamily = object.style.fontFamily;
+				document.getElementById("paneselectorbox"
+						+ paneselectorboxcount).style.fontSize = object.style.fontSize
+						+ 'px';
+				break;
+			}
+		}
+
 	}
 
 }
@@ -261,31 +306,47 @@ function funcStyleBox(object) {
  * input.
  */
 function funcRowBox(object) {
-	var i;
-	document.getElementById("main").innerHTML = "";
-	for (i = 0; i < object.contents.length; i++) {
-		switch (object.contents[i].type) {
-		case "TextBox":
-			funcTextBox(object.contents[i]);
-			break;
-		case "StyleBox":
-			funcStyleBox(object.contents[i]);
-			break;
-		case "TogglerButtonBox":
-			funcTogglerButtonBox(object.contents[i]);
-			break;
-		case "CheckboxBox":
-			funcCheckboxBoxOfRowBox(object.contents[i]);
-			break;
-		case "PaneSelectorBox":
-			contentTrueText = object.contents[i].contentTrue.text;
-			contentFalseText = object.contents[i].contentFalse.text;
-			funcPaneSelectorBox(object.contents[i]);
-			break;
-		case "RowBox":
-			funcRowBox(object.contents[i]);
-			break;
+	if (!object.hasOwnProperty('contents')) {
+		document.getElementById("main").innerHTML = "";
+		document.getElementById("error").innerHTML = "Please enter the value of contents for the RowBox. contents is an array of Boxdescriptor.";
+	} else {
+		if (Object.keys(object.contents).length === 0) {
+			document.getElementById("main").innerHTML = "";
+			document.getElementById("error").innerHTML = "contents array is empty.";
+		} else {
+			var i;
+			document.getElementById("main").innerHTML = "";
+			for (i = 0; i < object.contents.length; i++) {
+				switch (object.contents[i].type) {
+				case "TextBox":
+					funcTextBox(object.contents[i]);
+					break;
+				case "StyleBox":
+					funcStyleBox(object.contents[i]);
+					break;
+				case "TogglerButtonBox":
+					funcTogglerButtonBox(object.contents[i]);
+					break;
+				case "CheckboxBox":
+					funcCheckboxBoxOfRowBox(object.contents[i]);
+					break;
+				case "PaneSelectorBox":
+					if (object.contents[i].hasOwnProperty('contentTrue')
+							&& object.contents[i]
+									.hasOwnProperty('contentFalse')) {
+						contentTrueText = object.contents[i].contentTrue.text;
+						contentFalseText = object.contents[i].contentFalse.text;
+					}
+					funcPaneSelectorBox(object.contents[i]);
+					break;
+				case "RowBox":
+					funcRowBox(object.contents[i]);
+					break;
+				}
+			}
+			return this;
 		}
+
 	}
-	return this;
+
 }
